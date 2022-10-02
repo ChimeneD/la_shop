554b8d9d108008e5399a0a81240054f7058a78a1
Daniel Amadi <ama101@outlook.com>
2022-10-02 12:12:52 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -38,8 +38,9 @@ const MyApp = (props) => {
     uri: `${process.env.BACKEND_URI}`,
     fetchOptions: {
       mode: "no-cors",
+      credentials: "include",
     },
-    // credentials: "include",
+    credentials: "include",
     cache: new InMemoryCache(),
   });
 
